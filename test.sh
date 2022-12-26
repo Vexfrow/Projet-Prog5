c266ece9ea5b43fd42ec438a00cccf3c35e15520
Thomas Gregoire <Thomas.gregoire82@gmail.com>
2022-12-26 19:48:17 +0100

    Modification de la manière de faire les tests : affichage plus propre + comparaison lignes par lignes pour les tables

--- test.sh
+++ test.sh
@@ -18,36 +18,63 @@ testHeader(){
 
 #Revoir maniere de faire pour comparer ligne par ligne
 testSectionHeader(){
-    echo `arm-none-eabi-readelf -S -W $1 | head -n -5 | tail -n +5` > resultatAttendu   
-    echo `./main -sh $1 | tail -n +3 | sed -r 's/SHT_NULL/NULL/g' | sed -r 's/SHT_NULL/NULL/g' | sed -r 's/SHT_PROGBITS/PROGBITS/g' | sed -r 's/SHT_NOBITS/NOBITS/g' | sed -r 's/SHT_ARM_ATTR/ARM_ATTRIBUTES/g' | sed -r 's/SHT_SYMTAB/SYMTAB/g' | sed -r 's/SHT_STRTAB/STRTAB/g' | sed -r 's/SHT_REL/REL/g'`> resultatObtenu
-    echo `grep -o [a-zA-Z0-9.] resultatAttendu` > resultatAttendu
-    echo `grep -o [a-zA-Z0-9.] resultatObtenu` > resultatObtenu
-  
-    if diff resultatObtenu resultatAttendu
-    then    
-        echo "[HEADER SECTION] OK pour $1"
-    else
-        echo "[HEADER SECTION] ECHEC pour $1"
-        exit
-    fi
- 
+    arm-none-eabi-readelf -S -W $1 | head -n -5 | tail -n +5 | sed -r 's/ //g' | sed -r 's/\t//g' > resultatAttendu   
+    ./main -sh $1 | tail -n +3 | sed -r 's/SHT_//g' | sed -r 's/ARM_ATTR/ARM_ATTRIBUTES/g' | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
+
+    i=1
+    while read -r ligneA;
+    do
+        ligneO=`cat resultatObtenu | head -n $i | tail -n -1`
+
+        if [ "$ligneA" != "$ligneO" ]
+        then
+            echo "[SECTION HEADER] ECHEC pour $1 à la ligne $i "
+            echo ""
+            echo "Ligne attendu :"
+            echo "$ligneA"
+            echo ""
+            echo "Ligne Obtenu"
+            echo "$ligneO"
+            exit
+        fi
+
+
+
+        ((i+=1))
+    done < resultatAttendu
+    echo "[SECTION HEADER] OK pour $1"
+    
 }
 
 
 #Revoir maniere de faire pour comparer ligne par ligne
 testSymbolTable(){
-    echo `arm-none-eabi-readelf -s -W $1 | tail -n +4` > resultatAttendu   
-    echo `./main -st $1 | tail -n +2` > resultatObtenu
-    echo `grep -o [a-zA-Z0-9.$] resultatAttendu` > resultatAttendu
-    echo `grep -o [a-zA-Z0-9.$] resultatObtenu` > resultatObtenu
-  
-    if diff resultatObtenu resultatAttendu
-    then    
-        echo "[SYMBOL TABLE] OK pour $1"
-    else
-        echo "[SYMBOL TABLE] ECHEC pour $1"
-        exit
-    fi
+
+    arm-none-eabi-readelf -s -W $1 | tail -n +4 | sed -r 's/ //g' | sed -r 's/\t//g' > resultatAttendu   
+    ./main -st $1 | tail -n +2 | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
+
+    i=1
+    while read -r ligneA;
+    do
+        ligneO=`cat resultatObtenu | head -n $i | tail -n -1`
+
+        if [ "$ligneA" != "$ligneO" ]
+        then
+            echo "[SYMBOL TABLE] ECHEC pour $1 à la ligne $i "
+            echo ""
+            echo "Ligne attendu :"
+            echo "$ligneA"
+            echo ""
+            echo "Ligne Obtenu"
+            echo "$ligneO"
+            exit
+        fi
+
+
+
+        ((i+=1))
+    done < resultatAttendu
+    echo "[SYMBOL TABLE] OK pour $1"
  
 }
 
