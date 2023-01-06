c66e656d3a283b4905a76b38d3ff277c0d296425
Thomas Gregoire <Thomas.gregoire82@gmail.com>
2023-01-06 13:21:41 +0100

    MAJ script test

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ nbSections=0
 
 testHeader(){
     arm-none-eabi-readelf -h $1 > resultatAttendu
-    ./main -h $1 | head -n -1  > resultatObtenu
+    ./affichageElf -h $1 | head -n -1  > resultatObtenu
 
     if diff resultatObtenu resultatAttendu
     then    
@@ -18,7 +18,7 @@ testHeader(){
 
 testSectionHeader(){
     arm-none-eabi-readelf -S -W $1 | head -n -5 | tail -n +5 | sed -r 's/ //g' | sed -r 's/\t//g' > resultatAttendu   
-    ./main -S $1 | tail -n +3 | sed -r 's/SHT_//g' | sed -r 's/ARM_ATTR/ARM_ATTRIBUTES/g' | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
+    ./affichageElf -S $1 | tail -n +3 | sed -r 's/SHT_//g' | sed -r 's/ARM_ATTR/ARM_ATTRIBUTES/g' | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
 
     i=1
     while read -r ligneA;
@@ -55,7 +55,7 @@ testSectionHeader(){
 testSymbolTable(){
 
     arm-none-eabi-readelf -s -W $1 | tail -n +4 | sed -r 's/ //g' | sed -r 's/\t//g' > resultatAttendu   
-    ./main -s $1 | tail -n +2 | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
+    ./affichageElf -s $1 | tail -n +2 | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
 
     i=1
     while read -r ligneA;
@@ -94,7 +94,7 @@ testSectionUnique(){
     for j in `seq 0 $nbVar`
     do
         arm-none-eabi-readelf -x $j $1 | cut -c 1-48 | tail -n +3 | sed -r 's/ //g' | sed -r 's/\t//g' > resultatAttendu   
-        ./main -x $j $1 | cut -c 1-48 | tail -n +3 | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
+        ./affichageElf -x $j $1 | cut -c 1-48 | tail -n +3 | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
 
         if [[ `sed -n 1p resultatAttendu` == "NOTE:Thissectionhasrelocationsagainstit," ]]
         then 
@@ -120,7 +120,7 @@ testSectionUnique(){
 testRelocationTable(){
 
     arm-none-eabi-readelf -r -W $1 | tail -n +2 | sed -r 's/ //g' | sed -r 's/\t//g' > resultatAttendu   
-    ./main -r $1 | head -n -1 | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
+    ./affichageElf -r $1 | head -n -1 | sed -r 's/ //g' | sed -r 's/\t//g'> resultatObtenu
 
     i=1
     while read -r ligneA;
